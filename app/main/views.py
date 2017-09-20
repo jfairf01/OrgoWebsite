460462121459afa1a72f00859c1b47eccc8fa392
John Fairfield-Sonn <johntfairfieldsonn@gmail.com>
2017-09-20 12:00:38 -0400

    Created initial website boilerplate

---
+++ app/main/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from ..models import EditableHTML
+
+from . import main
+
+
+@main.route('/')
+def index():
+    return render_template('main/index.html')
+
+@main.route('/SecondPage')
+def SecondPage():
+    return render_template('main/SecondPage.html')
+
+@main.route('/about')
+def about():
+    editable_html_obj = EditableHTML.get_editable_html('about')
+    return render_template('main/about.html',
+                           editable_html_obj=editable_html_obj)
